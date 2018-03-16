58ba7e9572ceb5932b04c241c270e1ed4ea9d335
lanceplarsen <lancep.larsen43@gmail.com>
2018-03-15 21:39:45 -0700

    Update modules.tf

--- modules.tf
+++ modules.tf
@@ -1,3 +1,8 @@
+data "aws_route53_zone" "pcf_zone" {
+  name         = "hashidemos.io."
+}
+
+
 module "ops_manager" {
   source = "./ops_manager"
 
@@ -11,6 +16,6 @@ module "ops_manager" {
   subnet_id     = "${aws_subnet.public_subnets.0.id}"
   vpc_id        = "${aws_vpc.vpc.id}"
   dns_suffix    = "${var.dns_suffix}"
-  zone_id       = "${aws_route53_zone.pcf_zone.id}"
+  zone_id       = "${data.aws_route53_zone.pcf_zone.id}"
   iam_user_name = "${aws_iam_user.iam_user.name}"
 }
